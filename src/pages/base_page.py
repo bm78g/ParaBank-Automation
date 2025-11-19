38235e643e11e11dce43f16e3bc3c24bdece064a
bm78g <73971640+bm78g@users.noreply.github.com>
2025-11-19 15:49:43 -0600

    chore: init repository

---
+++ src/pages/base_page.py
@@ -0,0 +1,45 @@
+# Contains base functionalities for accessing pages and its elements.
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+class Page:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+    
+    def click(self, element):
+        self.wait.until(EC.element_to_be_clickable(element)).click()
+        
+    def get_element(self, *, id=None, class_name=None, name=None, xpath=None, text=None):
+        locator = None
+        if id is not None:
+            locator = (By.ID, id)
+        elif class_name is not None:
+            locator = (By.CLASS_NAME, class_name)
+        elif name is not None:
+            locator = (By.NAME, name)
+        elif xpath is not None:
+            locator = (By.XPATH, xpath)
+        elif text is not None:
+            xpath = (
+                f"//button[normalize-space(.)=\"{text}\"] | "
+                f"//a[normalize-space(.)=\"{text}\"] | "
+                f"//input[@value=\"{text}\"]"
+            )
+            locator = (By.XPATH, xpath)
+        else:
+            raise ValueError("get_element ran without sufficient parameters.")
+
+        return self.wait.until(EC.presence_of_element_located(locator))
+    
+    def get_text(self, element):
+        if isinstance(element, tuple):
+            elem = self.wait.until(EC.presence_of_element_located(element))
+        else:
+            elem = element
+        return elem.text
+    
+    def open_page(self, url):
+        self.driver.get(url)
---
+++ src/tests/test_login.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+import time
+
+from ..pages import base_page
+
+service = Service(executable_path="drivers/chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+login_page = base_page.Page(driver=driver)
+
+def main():
+    login_page.open_page("https://parabank.parasoft.com/parabank/index.htm")
+    assert "ParaBank" in driver.title
+
+    log_in(login_page=login_page)
+
+    time.sleep(5)
+    driver.close()
+
+def log_in(login_page):
+    username_input = login_page.get_element(name="username")
+    username_input.send_keys("username")
+
+    password_input = login_page.get_element(name="password")
+    password_input.send_keys("password")
+
+    login_button = login_page.get_element(text="Log In")
+    login_page.click(login_button)
+
+if __name__ == "__main__":
+    main()
